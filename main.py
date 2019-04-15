fe8aa90382d6b8ce3a0b28294e9a270824ab2a2e
biswasindhu.mandal <biswasindhu.mandal@rebaca.com>
2019-04-15 13:49:57 +0530

    generate logger file

--- main.py
+++ main.py
@@ -2,7 +2,17 @@
 import src.config as config
 import src.controller.controller as controller
 # @generate logger file
+import lib.logging as logging
 
+# ------------Rainfall--Data--Analysis------------------------
+LOG_FILENAME = 'logger/logger.log'
+logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG)
+
+logging.debug('This is a debug message')
+logging.info('This is an info message')
+logging.warning('This is a warning message')
+logging.error('This is an error message')
+logging.critical('This is a critical message')
 
 try :
     (choice, sms) = ('1', None)
